c6f914b79d9fbd06c9ea420a9bea78a41360f709
Stéphane Bouchard <59700933+partiexplorer@users.noreply.github.com>
2024-02-24 21:38:42 -0500

    Fix csv file selection

--- tools/convert.py
+++ tools/convert.py
@@ -17,9 +17,9 @@ def select_csv_to_convert():
     # Liste des fichiers GPX
     gpx_files = glob.glob(os.path.join(directory, "*.gpx"))
 
-    # Extraire les noms de fichiers sans nom de répertoire pour les fichiers CSV et GPX
-    csv_file_names = [os.path.basename(file) for file in csv_files]
-    gpx_file_names = [os.path.basename(file) for file in gpx_files]
+    # Extraire les noms de fichiers sans extension pour les fichiers CSV et GPX
+    csv_file_names = [os.path.splitext(os.path.basename(file))[0] for file in csv_files]
+    gpx_file_names = [os.path.splitext(os.path.basename(file))[0] for file in gpx_files]
 
     # Trouver les fichiers CSV qui n'ont pas de fichier GPX avec le même nom
     missing_gpx_files = [file for file in csv_file_names if file not in gpx_file_names]
@@ -27,12 +27,12 @@ def select_csv_to_convert():
 
     print("Fichiers CSV sans fichier GPX correspondant:")
     for i, file in enumerate(missing_gpx_files, start=1):
-        print(f"{i}. {file}")
+        print(f"{i}. {file}" + ".csv")
 
     selection = input("Sélectionnez un fichier pour traiter (ou 'q' pour quitter) : ")
     if selection.isdigit() and 1 <= int(selection) <= len(missing_gpx_files):
         selected_file = missing_gpx_files[int(selection) - 1]
-        return selected_file
+        return selected_file + ".csv"
     else:
         return None
 
